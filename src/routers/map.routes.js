44bdd6f41ea172a6b4893e58220480905b6b88df
Guilherme Carra <gui_gc@hotmail.com>
2020-12-04 13:33:53 +0100

    Add: near-people map route

--- src/routers/map.routes.js
+++ src/routers/map.routes.js
@@ -1,9 +1,14 @@
-require("dotenv").config();
 const express = require("express");
+const passport = require("passport");
+require("dotenv").config();
 
 const router = express.Router();
 const userController = require("../controllers/user-controller");
 
-router.post("/near-people", userController.nearPeople);
+router.post(
+  "/near-people",
+  passport.authenticate("bearer",{ session: false}),
+  userController.nearPeople
+);
 
 module.exports = router;
