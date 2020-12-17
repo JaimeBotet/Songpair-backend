72e6854aac19d388cb6a9dce55d07d1674d2b599
JaimeBotet <jaime.90.botet@gmail.com>
2020-12-17 11:30:53 +0100

    Comment socket io for dev purposes

--- src/app.js
+++ src/app.js
@@ -1,42 +1,42 @@
 require("dotenv").config();
 
 const app = require("./server");
-const socketio = require("socket.io");
+// const socketio = require("socket.io");
 
 
 const config = require("./config/app-config")[process.env.NODE_ENV || "development"];
 const connect = require("./db/connect");
-const io = socketio(app);
+// const io = socketio(app);
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 
 
 //SOCKET IO HANDLERS
-io.on('connect', (socket) => {
-  socket.on('join', ({ user, room }, callback) => {
+// io.on('connect', (socket) => {
+//   socket.on('join', ({ user, room }, callback) => {
 
-    //it will be announced when each participant "joins the room"
-    socket.join(room);
+//     //it will be announced when each participant "joins the room"
+//     socket.join(room);
 
-    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${room}.`});
-    socket.broadcast.to(room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+//     socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${room}.`});
+//     socket.broadcast.to(room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
 
-    callback();
-  });
+//     callback();
+//   });
 
-  socket.on('sendMessage', ({user, room, message}, callback) => {
-    io.to(room).emit('message', { user: user.name, text: message });
+//   socket.on('sendMessage', ({user, room, message}, callback) => {
+//     io.to(room).emit('message', { user: user.name, text: message });
 
-    callback();
-  });
+//     callback();
+//   });
 
-  socket.on('disconnect', ({ name, room }) => {
-    //it will be announced when each participant "leaves the room"
-    io.to(room).emit('message', { user: 'Admin', text: `${name} has left.` });
-  })
-});
+//   socket.on('disconnect', ({ name, room }) => {
+//     //it will be announced when each participant "leaves the room"
+//     io.to(room).emit('message', { user: 'Admin', text: `${name} has left.` });
+//   })
+// });
 
 
 
