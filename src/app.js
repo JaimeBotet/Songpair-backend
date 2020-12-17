19f15ccaa515a8c312b16f7f9248afc37db381ab
JaimeBotet <jaime.90.botet@gmail.com>
2020-12-17 18:20:08 +0100

    Fix Cors Problem and Clean socket code

--- src/app.js
+++ src/app.js
@@ -1,50 +1,24 @@
 require("dotenv").config();
 
 const app = require("./server");
-// const socketio = require("socket.io");
-
+const socketCon = require('./utils/socket');
 
 const config = require("./config/app-config")[process.env.NODE_ENV || "development"];
 const connect = require("./db/connect");
-// const io = socketio(app);
+
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 
 
-//SOCKET IO HANDLERS
-// io.on('connect', (socket) => {
-//   socket.on('join', ({ user, room }, callback) => {
-
-//     //it will be announced when each participant "joins the room"
-//     socket.join(room);
-
-//     socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${room}.`});
-//     socket.broadcast.to(room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
-
-
-//     callback();
-//   });
-
-//   socket.on('sendMessage', ({user, room, message}, callback) => {
-//     io.to(room).emit('message', { user: user.name, text: message });
-
-//     callback();
-//   });
-
-//   socket.on('disconnect', ({ name, room }) => {
-//     //it will be announced when each participant "leaves the room"
-//     io.to(room).emit('message', { user: 'Admin', text: `${name} has left.` });
-//   })
-// });
-
-
-
 connect()
-  .then(async () => {
-    app.listen(port, host, () => {
-        console.log(`Server listening on http://localhost:${config.app.port}`);
+  .then(() => {
+    const server = app.listen(port, host, () => {
+      console.log(`Server listening on http://localhost:${config.app.port}`);
     });
+
+    socketCon(server);
+
   })
   .catch((error) => {
     console.error(error);
---
+++ src/utils/socket.js
@@ -0,0 +1,38 @@
+const config = require('../config/app-config')[process.env.NODE_ENV || "development"];
+const socketio = require("socket.io");
+
+const socketCon = (server) => {
+    console.log("Entered socketCon");
+    const io = socketio(server, {
+        cors: {
+            origin: config.app.clientDomain,
+            credentials: true
+        }
+    });
+
+    io.on('connection', (socket) => {
+        console.log("Made Socket connection");
+        socket.on('join', ({ user, room }, callback) => {
+        //it will be announced when each participant "joins the room"
+        socket.join(room);
+        //here we send the room number/id to the other user
+    
+        socket.broadcast.to(room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+    
+        callback();
+        });
+    
+        //When a user in the frontend "sends" a message, we broadcast it back to all the users in the room
+        socket.on('sendMessage', ({user, room, message}, callback) => {
+            io.to(room).emit('message', { user: user.name, text: message });
+            callback();
+        });
+    
+        //it will be announced when each participant "leaves the room"
+        socket.on('disconnect', ({ name, room }) => {
+        io.to(room).emit('message', { user: 'Admin', text: `${name} has left.` });
+        })
+    });
+}
+
+module.exports = socketCon;
