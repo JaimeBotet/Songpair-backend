a5febc761b2d48014592c18ee181b5786a80a3a4
JaimeBotet <jaime.90.botet@gmail.com>
2020-12-21 20:17:27 +0100

    Remove redirect (done in frontend)

--- src/controllers/room-controller.js
+++ src/controllers/room-controller.js
@@ -19,20 +19,16 @@ async function openRoom(req, res, next) {
         receiver,
     } = req.body;
     
-    console.log("Received request to open chat with participant: " + receiver);
     //Information sent from the frontend:
     //user -> token
     //participant -> spotifyID
     const creator = await db.User.findOne({ token: sender }).catch(next);
     const participant =  await db.User.findOne({ spotifyID: receiver }).catch(next);
-    console.log(creator._id);
-    console.log(participant._id);
 
     //We check if there is a chat room opened already, created either by the user or the other participant
     const room = await db.Room.findOne({ $or:[{creatorID:creator._id , participantID:participant._id}, {creatorID:participant._id , participantID:creator._id} ]}).catch(next);
-    
-    // if(room) return res.status(200).send({data: room._id, error: null});
-    if(room) return res.redirect(config.app.clientDomain + "/chat/" + room._id, {data: room._id});
+
+    if(room) return res.status(200).send({data: room._id, error: null});
 
     const newRoom = await db.Room.create({
         creatorID: creator._id,
@@ -41,7 +37,7 @@ async function openRoom(req, res, next) {
         return next(error);
     });
 
-    return res.redirect(config.app.clientDomain + "/chat/" + newRoom._id, {data: newRoom._id});
+    return res.status(200).send({data: newRoom._id, error: null});
 }
 
 module.exports = {
