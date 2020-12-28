a61f1afc2211302e8900dd724b0b7d762efd900b
Guilherme Carra <gui_gc@hotmail.com>
2020-12-28 14:38:47 +0100

    Add: socket notification event

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 const config = require('../config/app-config')[process.env.NODE_ENV || "development"];
 const socketio = require("socket.io");
-
+const socketController = require("../controllers/socket-controller")
 
 const socketCon = (server) => {
     const io = socketio(server, {
@@ -10,12 +10,17 @@ const socketCon = (server) => {
         }
     });
 
-    io.on('connection', (socket) => {
-        
-        
-        socket.on('join', ({ user, room }, callback) => {
+    io.on('connection', async (socket) => {
+        if (socket.handshake.query['token']) await socketController.updateSocket(socket.handshake.query['token'], socket.id);
+
+        socket.on('newChat', async ({ sender, receiver }) => {
+            const receiverSocket = await socketController.getSocket(receiver);
+            io.to(receiverSocket).emit("newMessage", {sender})
+        });
+
+        /* socket.on('join', ({ user, room }, callback) => {
             socket.join(room);
-            
+
             //We notify the frontend saying a new socket connection has been made. In the socket we filter if it applies to us
             socket.broadcast.emit('connection', {user:user, room: room});
 
@@ -24,17 +29,17 @@ const socketCon = (server) => {
             socket.broadcast.to(room).emit('message', { user: 'Server', text: `${user.name} has joined!` });
             callback();
         });
-    
+
         //When a user in the frontend "sends" a message, we broadcast it back to the room
         socket.on('sendMessage', ({user, room, message}, callback) => {
             io.to(room).emit('message', { user: user.name, text: message });
             callback();
         });
-    
+
         //it will be announced when each participant "leaves the room"
         socket.on('leaveChat', ({ user, room }) => {
         io.to(room).emit('message', { user: 'Server', text: `${user.name} has left.` });
-        })
+        }) */
     });
 }
 
