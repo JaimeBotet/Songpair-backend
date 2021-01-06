4e1969eaf79fbb20d7a263c714534a33cfe1fe5d
JaimeBotet <jaime.90.botet@gmail.com>
2021-01-06 22:42:21 +0100

    some change

--- src/__tests__/like.test.js
+++ src/__tests__/like.test.js
@@ -1,35 +1,45 @@
-import controller from "../controllers/like-controller";
+import { getMusicLikes, getProfileLikes,} from "../controllers/like-controller";
 
 /**
  * Finish the test
  */
 describe("like test-suite", () => {
-  let addSpy = null;
 
-  afterAll(() => {
-    if (addSpy.hasOwnProperty("mockRestore")) {
-      addSpy.mockRestore();
+    const user = {
+        receiverID = "1234"
     }
+
+  it("get the Likes of a song", () => {
+    expect(getMusicLikes).toEqual("something");
   });
 
-  beforeAll(() => {
-    // 1. Spy on the 'add' method of the calculator object
-    //    and save the spy in the `addSpy` variable
-    addSpy = jest.spyOn(calculator, "add");
-  });
+  it("get the likes of a profile", () => {
+      expect(getProfileLikes).toEqual("something");
+  })
 
-  test("increment calls the calculator add method", () => {
-    /**
-     * 2. Execute the `increment` by passing the arguments: (4, 5, calculator)
-     */
-    increment(4, 5, calculator);
-    //
-    // 3. Make an assertion that the addSpy has been called 1 time
-    //
-    expect(addSpy).toHaveBeenCalledTimes(1);
-    // 4. Make an assertion that the addSpy has been called with the
-    // numbers 4 and 5
-    //
-    expect(addSpy).toHaveBeenCalledWith(4, 5);
+
+  //example tests & functions
+
+  //Example 1
+  function sanitizeUserData(user) {
+    const { password, role, ...rest } = user;
+    return rest;
+  }
+  test("sanitizeUserData returns an object without sensitive information", () => {
+    const safeUserData = {
+      firstName: "Alex",
+      lastName: "Marks",
+      age: 20,
+      jobTitle: "Developer",
+    };
+  
+    const userWithSensitiveInformation = {
+      ...safeUserData,
+      password: "a98dsj9a8sdj89asd89jasd",
+      role: "ADMIN",
+    };
+  
+    // Finish the test
+    expect(sanitizeUserData(userWithSensitiveInformation)).toEqual(safeUserData);
   });
 });
